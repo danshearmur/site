d07ac5900bb2ce1135b0fd2e59ae4e44d8a18444
Dan Shearmur <dan@shearmur.co.uk>
2012-03-13 21:33:57 +0000

    Broke up JS into base and desktop functionality

--- build.sh
+++ build.sh
@@ -2,13 +2,18 @@
 TMPL_FOLDER="tmpl/"
 JS_FOLDER="js/"
 TMPL="templates"
-OUTPUT="output.js"
+BASE="base.js"
+DESKTOP="desktop.js"
 CSS_FOLDER="css/"
 CSS_FILE="dan.less"
-rm $JS_FOLDER$OUTPUT
+rm $JS_FOLDER$BASE $JS_FOLDER$DESKTOP
 tplcpl -t $TMPL_FOLDER -o $JS_FOLDER${TMPL}.js
-SCRIPTS="raphael-min twitter-text moment ${TMPL} app"
+SCRIPTS="raphael-min app"
+DESKTOP_SCRIPTS="twitter-text moment ${TMPL} extra"
 for script in $SCRIPTS; do
-  uglifyjs $JS_FOLDER$script.js >> $JS_FOLDER$OUTPUT
+  uglifyjs $JS_FOLDER$script.js >> $JS_FOLDER$BASE
+done
+for script in $DESKTOP_SCRIPTS; do
+  uglifyjs $JS_FOLDER$script.js >> $JS_FOLDER$DESKTOP
 done
 lessc $CSS_FOLDER$CSS_FILE $CSS_FOLDER$CSS_FILE.min.css -compress
--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function () {
 
-  var body, header, aside, svgs, jid;
+  var body, header, svgs;
 
   function getFirst(el) {
     return document.getElementsByTagName(el)[0];
@@ -8,7 +8,6 @@
 
   body   = getFirst('body');
   header = getFirst('header');
-  aside  = getFirst('aside');
 
   svgs = [];
 
@@ -36,59 +35,6 @@
     setTimeout(arguments.callee, 2.5 * 60 * 1000);
 
   }) ();
-
-  jid = 0;
-
-  function getJsonp(url, callback, jsonp) {
-
-    var jsonp, tag, full_url, callback_name, ref;
-
-    if (typeof callback != 'function') {
-      return false;
-    }
-    jsonp = jsonp || 'callback';
-
-    tag = document.createElement('script');
-    callback_name = 'j_' + (jid++);
-
-    window[callback_name] = function () {
-      callback(arguments);
-      delete window[callback_name];
-      tag.parentNode.removeChild(tag);
-    }
-
-    tag.src = url +
-      (url.indexOf('?') != -1 ? '&' : '?') +
-      jsonp + '=' + callback_name;
-    tag.id = callback_name;
-
-    ref = document.getElementsByTagName('script')[0];
-    ref.parentNode.insertBefore(tag, ref);
-
-    return true;
-  }
-
-  function appendHtml(el, html) {
-    var curr = el.innerHTML
-    el.innerHTML = curr + html;
-  }
-
-  function render() {
-    var locals, html;
-    locals = {
-      data: arguments[1][0]
-    };
-    html = Templating.tpl(arguments[0] + '.jade', locals);
-    appendHtml(aside, html);
-  }
-
-  getJsonp('//feeds.delicious.com/v2/json/danshearmur?count=5', function () {
-    render('delicious', arguments[0]);
-  });
-
-  getJsonp('//api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=danshearmur&count=5', function () {
-    render('twitter', arguments[0]);
-  });
 
   function getColour() {
     var output;
